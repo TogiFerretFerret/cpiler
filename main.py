37d1848b52a559c70d94b2655819401cb741f82d
river <hudsonreich@hunterschools.org>
2024-03-06 10:48:47 -0500

    hi

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ interman = interpac.InterMan()
 questionformer = qformer.QFormer()
 textconv = utils.tts.TextConverter()
 player = soundplay.SoundPlayer()
+pts = 0
 textconv.convert("Time is up!", "timeup.mp3")
+textconv.convert("Correct!", "correct.mp3")
+textconv.convert("Incorrect!", "incorrect.mp3")
+textconv.convert("Early answer!", "earlyanswer.mp3")
 def doquestion():
     # Get the question
     jsond = interman.sendGet("https://scibowldb.com/api/questions/random").json()
@@ -23,10 +27,57 @@ def doquestion():
     textconv.convert(qtext, "question.mp3")
     player.play("question.mp3")
     # Form answer
-    time.sleep(5)
-    player.play("timeup.mp3")
+    earlyanswer = False
+    for i in range(50):
+        if inputmanager.get() == " ":
+            earlyanswer = True
+            break
+        time.sleep(0.1)
+    if not earlyanswer:
+        player.play("timeup.mp3")
+    else:
+        player.play("earlyanswer.mp3")
     # Wait for kp
     inputmanager.waitforreturn()
     textconv.convert(qanswer, "answer.mp3")
     player.play("answer.mp3")
+    # Wait for kp
+    inp = inputmanager.getchars(chars=["y", "n"])
+    if inp == "y":
+        pts += tossup.ptworth
+        player.play("correct.mp3")
+        # Form bonus
+        qtext = questionformer.createQText(bonus, bonus.format, bonus.question)
+        qanswer = questionformer.createQAnswer(bonus.answer)
+        # Play bonus
+        textconv.convert(qtext, "question.mp3")
+        player.play("question.mp3")
+        # Form answer
+        earlyanswer = False
+        for i in range(20):
+            if inputmanager.get() == " ":
+                earlyanswer = True
+                break
+            time.sleep(1)
+        if not earlyanswer:
+            player.play("timeup.mp3")
+        else:
+            player.play("earlyanswer.mp3")
+        # Wait for kp
+        inputmanager.waitforreturn()
+        textconv.convert(qanswer, "answer.mp3")
+        player.play("answer.mp3")
+        # Wait for kp
+        inp = inputmanager.getchars(chars=["y", "n"])
+        if inp == "y":
+            pts += bonus.ptworth
+            player.play("correct.mp3")
+        else:
+            player.play("incorrect.mp3")
+    else:
+        player.play("incorrect.mp3")
+        if earlyanswer:
+            pts -= tossup.ptworth
+    
+        
 doquestion()
